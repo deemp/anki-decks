5224c9d4989ea2d3cd18595e8c61f5ee152f3377
Danila Danko <br4ch1st0chr0n3@gmail.com>
2025-01-02 19:46:09 +0300

    fix(frequency-de-en): update scripts to support more decks

--- frequency/de/scripts/main.py
+++ frequency/de/scripts/main.py
@@ -5,6 +5,9 @@ import pandas as pd
 
 os.chdir(f'{os.environ["ROOT_DIR"]}/frequency/de')
 
+FREQUENCY_CUTOFF_1 = 1700
+FREQUENCY_CUTOFF_2 = 3400
+
 # %%
 
 
@@ -13,10 +16,17 @@ def partition_deck_data():
 
     deck_full = pd.read_csv("de-en/deck.csv", sep="|", index_col=0)
     deck_full.index = deck_full.index.map(lambda x: x if x % 1 != 0 else f"{int(x)}")
-    deck_part_1 = deck_full.loc[deck_full["frequency_rank"] <= 3000]
-    deck_part_2 = deck_full.loc[deck_full["frequency_rank"] > 3000]
-    deck_part_1.to_csv("de-en/deck-1.csv", sep="|")
-    deck_part_2.to_csv("de-en/deck-2.csv", sep="|")
+
+    frequency_rank = deck_full["frequency_rank"]
+
+    deck_part_1 = deck_full.loc[frequency_rank <= FREQUENCY_CUTOFF_1]
+    deck_part_2 = deck_full.loc[
+        (frequency_rank > FREQUENCY_CUTOFF_1) & (frequency_rank <= FREQUENCY_CUTOFF_2)
+    ]
+    deck_part_3 = deck_full.loc[frequency_rank > FREQUENCY_CUTOFF_2]
+
+    for i, deck in enumerate([deck_part_1, deck_part_2, deck_part_3]):
+        deck.to_csv(f"de-en/deck-{i + 1}.csv", sep="|")
 
 
 partition_deck_data()
@@ -35,23 +45,37 @@ def render_ankiweb_descriptions():
     template = read_template()
 
     def write_template(
-        template: str, template_path_suff: str, replacement: dict[str, str]
+        template_path_suff: str,
+        replacement: dict[str, str],
+        template: str = str(template),
     ):
         for k, v in replacement.items():
             template = template.replace(f"{{{{{k}}}}}", v)
         with open(mk_template_path(template_path_suff), "w", encoding="UTF-8") as t:
             t.write(template)
 
-    write_template(
-        str(template),
-        "-1",
-        {"n_words": "the 1st to the 3000th", "deck_id": "1946034909"},
-    )
-    write_template(
-        str(template),
-        "-2",
-        {"n_words": "the 3001st to the 5000th", "deck_id": "763225563"},
-    )
+    deck_full = pd.read_csv("de-en/deck.csv", sep="|", index_col=0)
+    max_frequency = max(deck_full["frequency_rank"])
+
+    ids = ["1946034909", "763225563", "???"]
+
+    n_words = [
+        f"the 1st to the {FREQUENCY_CUTOFF_1}th",
+        f"the {FREQUENCY_CUTOFF_1 + 1}st to the {FREQUENCY_CUTOFF_2}th",
+        f"the {FREQUENCY_CUTOFF_1 + 1}st to the {max_frequency}th",
+    ]
+
+    for i, n_words_cur in enumerate(n_words):
+        ids_cur = [k for j, k in enumerate(ids) if j != i]
+
+        replacement = {"n_words": n_words_cur} | {
+            f"another_part_id_{j + 1}": id_cur for j, id_cur in enumerate(ids_cur)
+        }
+
+        write_template(
+            template_path_suff=f"-{i + 1}",
+            replacement=replacement,
+        )
 
 
 render_ankiweb_descriptions()
